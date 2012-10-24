2231e7572ac873cd0875e4471e920ba4efa6366c
Tomas Lori <tomas_lori142@hotmail.com>
2012-10-24 20:21:39 -0300

    defines

--- include/defs.h
+++ include/defs.h
@@ -20,6 +20,11 @@
 #define ACS_INT_386 	0x0E		/* Interrupt GATE 32 bits */
 #define ACS_INT         ( ACS_PRESENT | ACS_INT_386 )
 
+#define ENTER			00
+#define TAB				00
+#define BACKSPACE		00
+#define BACK_ARROW		00
+#define FORW_ARROW		00
 
 #define ACS_CODE        (ACS_PRESENT | ACS_CSEG | ACS_READ)
 #define ACS_DATA        (ACS_PRESENT | ACS_DSEG | ACS_WRITE)
