7a0126d594fb910f2be0946ca46a1e0c7c480447
Tomas Lori <tomas_lori142@hotmail.com>
2012-10-24 18:28:37 -0300

    first commit

---
+++ kernel.c
@@ -0,0 +1,70 @@
+#include "../include/kasm.h"
+#include "../include/defs.h"
+
+DESCR_INT idt[0xA];			/* IDT de 10 entradas*/
+IDTR idtr;				/* IDTR */
+
+int tickpos=200;
+
+void int_08() {
+
+    char *video = (char *) 0xb8000;
+    video[tickpos+=2]='+';
+
+}
+
+
+char keyboard_flags[4];
+char keyboard_buffer;
+
+void int_09() {
+
+	char aux= /* aca llamo a la rutina q levanta de i/o 060h y retorna el valor */;
+	
+	/* chequeo la matriz etc etc y pongo el valor final en el keyboard_buffer */
+	
+	
+}
+
+	
+/**********************************************
+kmain() 
+Punto de entrada de código C.
+*************************************************/
+
+kmain() 
+{
+
+        int i,num;
+
+/* Borra la pantalla. */ 
+
+	k_clear_screen();
+
+
+/* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ0    */
+
+        setup_IDT_entry (&idt[0x08], 0x08, (dword)&_int_08_hand, ACS_INT, 0);
+	
+/* Carga de IDTR    */
+
+	idtr.base = 0;  
+	idtr.base +=(dword) &idt;
+	idtr.limit = sizeof(idt)-1;
+	
+	_lidt (&idtr);	
+
+	_Cli();
+/* Habilito interrupcion de timer tick*/
+
+        _mascaraPIC1(0xFE);
+        _mascaraPIC2(0xFF);
+        
+	_Sti();
+
+        while(1)
+        {
+        }
+	
+}
+
